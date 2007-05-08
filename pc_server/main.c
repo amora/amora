6a61c355d4b54fe879e4ccee8275563ca47febea
adedasil <adedasil@1d5b794b-e733-0410-af9d-65fb28a70ecb>
2007-05-08 20:27:10 +0000

    Using select to detect client_socket new input. Now the app is able to handle continuously cellphone events (before it only worked once...).

--- pc_server/main.c
+++ pc_server/main.c
@@ -32,6 +32,8 @@ int main(int argc, char* argv[])
 {
 	Display *own_display = NULL;
 	int bytes_read = 0, server_socket, client_socket, channel = 1, res;
+	fd_set fd_set_socket;
+	struct timeval time_socket;
 	struct sockaddr_rc rem_addr = { 0 };
 	unsigned int opt = sizeof(rem_addr);
 	const int BUF_SIZE = 300;
@@ -65,20 +67,29 @@ int main(int argc, char* argv[])
 
 	while (1) {
 
+		printf("Entering main loop...\n");
 		client_socket = accept(server_socket, (struct sockaddr *)&rem_addr, &opt);
-		ba2str(&rem_addr.rc_bdaddr, buf );
-		fprintf(stderr, "accepted connection from %s\n", buf);
-		bytes_read = read_socket(client_socket, buf, BUF_SIZE);
-		printf("%s\t%d\n", buf, bytes_read);
-		/* Remember that there is the CMD_BREAK character at end of
-		 * command!
-		 */
-		--bytes_read;
-		res = ecell_convert_ewindow(buf, bytes_read);
-		if (res == NONE)
-			printf("Invalid event!\n");
-		else
-			send_event(KeyPress, x_key_code[res], own_display);
+
+		FD_ZERO(&fd_set_socket);
+		FD_SET(client_socket, &fd_set_socket);
+		time_socket.tv_sec = 5;
+		time_socket.tv_usec = 0;
+			while (select(1, &fd_set_socket, NULL, NULL, &time_socket) != -1) {
+				ba2str(&rem_addr.rc_bdaddr, buf );
+				fprintf(stderr, "accepted connection from %s\n", buf);
+
+				bytes_read = read_socket(client_socket, buf, BUF_SIZE);
+				printf("%s\t%d\n", buf, bytes_read);
+				/* Remember that there is the CMD_BREAK character at end of
+				 * command!
+				 */
+				--bytes_read;
+				res = ecell_convert_ewindow(buf, bytes_read);
+				if (res == NONE)
+					printf("Invalid event!\n");
+				else
+					send_event(KeyPress, x_key_code[res], own_display);
+			}
 
 		close(client_socket);
 
