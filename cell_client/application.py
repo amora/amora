f016b3eb42aad4085eec650d1f42afd4b76b7922
adedasil <adedasil@1d5b794b-e733-0410-af9d-65fb28a70ecb>
2007-06-14 18:46:47 +0000

    Making the code more modular (most of the functions related to main app is in the new class Application).
    Also added to soft menus for connect and start of operation.

---
+++ cell_client/application.py
@@ -0,0 +1,105 @@
+
+'''
+/*  Copyright (C) 2007  Adenilson Cavalcanti <savagobr@yahoo.com>
+ *
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; by version 2 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+'''
+
+import appuifw
+import e32
+from keyboard import *
+from bt_client import *
+
+class application:
+    mouse_x = 400
+    mouse_y = 400
+    delta = 10
+    wallpaper = None
+    presentation = None
+    running = 0
+    bt = None
+    keyboard = None
+    #Add another object for 'options' window
+    def __init__(self):
+        self.wallpaper = appuifw.Canvas(None, None)
+        appuifw.app.menu = [(u'start', self.start), (u'connect', self.__connect)]
+                            #(u'options', lambda:None)]
+        appuifw.app.exit_key_handler = self.quit
+        self.keyboard = Keyboard()
+        #Cleanup the screen
+        appuifw.app.body = self.wallpaper
+        self.wallpaper.clear()
+        appuifw.app.title = u'P4X'
+        self.presentation = appuifw.Canvas(event_callback =
+                                           self.keyboard.handle_event,
+                                           redraw_callback = None)
+    #Exit function
+    def quit(self):
+        self.running = 0
+        self.bt.close()
+        appuifw.app.set_exit()
+    #Private function, will try to connect with a server PC
+    #using bluetooth
+    def __connect(self):
+        self.bt = bt_client()
+        self.bt.connect()
+    #Start presentation mode
+    def start(self):
+        self.running = 1
+        appuifw.app.body = self.presentation
+        appuifw.app.exit_key_handler = self.quit
+        if self.keyboard.is_down(EScancode6):
+            print u'RIGHT'
+            self.bt.write_line(u'RIGHT')
+        elif self.keyboard.is_down(EScancode4):
+            print u'LEFT'
+            self.bt.write_line(u'LEFT')
+        elif self.keyboard.is_down(EScancode2):
+            print u'UP'
+            self.bt.write_line(u'UP')
+        elif self.keyboard.is_down(EScancode8):
+            print u'DOWN'
+            self.bt.write_line(u'DOWN')
+        elif self.keyboard.is_down(EScancodeUpArrow):
+            print u'MOUSE_UP'
+            self.mouse_y = self.mouse_y - self.delta
+            self.bt.write_line(u'MOUSE_MOVE')
+            self.bt.write_line(str(self.mouse_x))
+            self.bt.write_line(str(self.mouse_y))
+        elif self.keyboard.is_down(EScancodeDownArrow):
+            print u'MOUSE_DOWN'
+            self.mouse_y = self.mouse_y + self.delta
+            self.bt.write_line(u'MOUSE_MOVE')
+            self.bt.write_line(str(self.mouse_x))
+            self.bt.write_line(str(self.mouse_y))
+        elif self.keyboard.is_down(EScancodeLeftArrow):
+            print u'MOUSE_LEFT'
+            self.mouse_x = self.mouse_x - self.delta
+            self.bt.write_line(u'MOUSE_MOVE')
+            self.bt.write_line(str(self.mouse_x))
+            self.bt.write_line(str(self.mouse_y))
+        elif self.keyboard.is_down(EScancodeRightArrow):
+            print u'MOUSE_RIGHT'
+            self.mouse_x = self.mouse_x + self.delta
+            self.bt.write_line(u'MOUSE_MOVE')
+            self.bt.write_line(str(self.mouse_x))
+            self.bt.write_line(str(self.mouse_y))
+
+
+
+
+
--- cell_client/presenter.py
+++ cell_client/presenter.py
@@ -18,74 +18,15 @@
  *
  */
 '''
-
-sys.path.append('E:\\python\\libs\\')
-import socket
 import appuifw
 import e32
-from keyboard import *
-from bt_client import *
 
+sys.path.append('E:\\python\\libs\\')
+from application import *
 
-def quit():
-    global running
-    running=0
-    appuifw.app.set_exit()
-
-
-
-bt = bt_client()
-keyboard = Keyboard()
-
-running = 1
-canvas = appuifw.Canvas(event_callback = keyboard.handle_event, redraw_callback=None)
-mouse_x = 400
-mouse_y = 400
-delta = 10
-
-appuifw.app.body = canvas
-appuifw.app.exit_key_handler = quit
-
-bt.connect()
-while running:
-    if keyboard.is_down(EScancode6):
-        print u'RIGHT'
-        bt.write_line(u'RIGHT')
-    elif keyboard.is_down(EScancode4):
-        print u'LEFT'
-        bt.write_line(u'LEFT')
-    elif keyboard.is_down(EScancode2):
-        print u'UP'
-        bt.write_line(u'UP')
-    elif keyboard.is_down(EScancode8):
-        print u'DOWN'
-        bt.write_line(u'DOWN')
-    elif keyboard.is_down(EScancodeUpArrow):
-        print u'MOUSE_UP'
-        mouse_y = mouse_y - delta
-        bt.write_line(u'MOUSE_MOVE')
-        bt.write_line(str(mouse_x))
-        bt.write_line(str(mouse_y))
-    elif keyboard.is_down(EScancodeDownArrow):
-        print u'MOUSE_DOWN'
-        mouse_y = mouse_y + delta
-        bt.write_line(u'MOUSE_MOVE')
-        bt.write_line(str(mouse_x))
-        bt.write_line(str(mouse_y))
-    elif keyboard.is_down(EScancodeLeftArrow):
-        print u'MOUSE_LEFT'
-        mouse_x = mouse_x - delta
-        bt.write_line(u'MOUSE_MOVE')
-        bt.write_line(str(mouse_x))
-        bt.write_line(str(mouse_y))
-    elif keyboard.is_down(EScancodeRightArrow):
-        print u'MOUSE_RIGHT'
-        mouse_x = mouse_x + delta
-        bt.write_line(u'MOUSE_MOVE')
-        bt.write_line(str(mouse_x))
-        bt.write_line(str(mouse_y))
+app = application()
+flag = 1
+while flag:
+    if (app.running == 1):
+        app.start()
     e32.ao_yield()
-
-
-
-bt.close()
