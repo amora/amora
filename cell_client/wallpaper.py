90c7e6eac3dde9d1354fb5f5026dae8f099463fe
adedasil <adedasil@1d5b794b-e733-0410-af9d-65fb28a70ecb>
2007-06-15 19:19:35 +0000

    Fixing resolution dependency wallpaper display.
    Now it works fine in several resolutions:
     - N93 (240, 320)
     - E61 (320, 240)
     - N70 (176, 208)

--- cell_client/wallpaper.py
+++ cell_client/wallpaper.py
@@ -3,12 +3,10 @@ import appuifw
 import keyboard
 
 class wallpaper:
-    def __init__(self, width = 320, height = 240):
+    def __init__(self):
         self.keyboard = keyboard.Keyboard()
+        self.image_obj = self.load_image()
         #TODO: redraw works, but is a bit messy.
-        self.width = width
-        self.height = height
-        self.image_obj = graphics.Image.open('E:\\python\\imgs\\wallpaper.jpg')
         self.canvas = appuifw.Canvas(event_callback = self.keyboard.handle_event,
                                      redraw_callback = self.event_redraw)
     def display(self):
@@ -17,14 +15,26 @@ class wallpaper:
         self.canvas.blit(self.image_obj, target = tmp)
     def event_redraw(self, other):
         self.display()
-    #FIXME: still doesn't work for N93
+    #Calculates position where to blit the image in Canvas
     def calc_position(self):
-        x = 0; y = 0;
-        if (self.width > self.image_obj.size[0]):
-            x = (self.width - self.image_obj.size[0])/2
-        if (self.height > self.image_obj.size[1]):
-            y = (self.height - self.image_obj.size[1])/2
-        return [x, y]
+        #this magic value works for N93, E61, N70
+        target = [0, -10]
+        return target
+    #Calculates aspect ratio and resize original image
+    def load_image(self):
+        canvas = appuifw.Canvas(None, None)
+        screen_size = canvas.size
+        canvas = None
+        border_perc = None
+        #FIXME: location dependent code!
+        img = graphics.Image.open('E:\\python\\imgs\\wallpaper_wide.jpg')
+        ratio_x = screen_size[0]/float(img.size[0])
+        ratio_y = screen_size[1]/float(img.size[1])
+        print [ratio_x, ratio_y]
+        scaled = img.resize([(img.size[0] * ratio_x),
+                                 (img.size[1] * ratio_y)], keepaspect = 1)
+        return scaled
+
 
 # obj = wallpaper()
 # appuifw.body = obj.canvas
