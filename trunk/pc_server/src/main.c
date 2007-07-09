2f50935199a5fbb48d74bd589c6b5149f16882d9
cavalcantii <cavalcantii@1d5b794b-e733-0410-af9d-65fb28a70ecb>
2007-07-09 12:12:04 +0000

    Updating todos: autootols buildsystem is working fine.

--- trunk/pc_server/src/main.c
+++ trunk/pc_server/src/main.c
@@ -12,7 +12,6 @@
  * - new background/icon for connected status
  * - refactor main.c, its starting to get smelly
  * - screenshot from active window.
- * - autotools buildsystem.
  * - logging feature.
  * - catch SIGTERM or provide a way to clean exit (close sockets).
  * - forking the process when new connection is received can be a good idea.
