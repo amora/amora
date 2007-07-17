ee9fdc9853931b31362af0b7edadc4a8763dc35e
cavalcantii <cavalcantii@1d5b794b-e733-0410-af9d-65fb28a70ecb>
2007-07-17 14:54:59 +0000

    Adding copyright remarks.

--- trunk/pc_server/src/log.h
+++ trunk/pc_server/src/log.h
@@ -1,6 +1,7 @@
 /**
  * @file   log.h
  * @author Vanderson Rodrigues
+ * @author Adenilson Cavalcanti
  * @date  Tue Jul 10 16:09:20 2007
  *
  * @brief Amora Log system definitions.
@@ -11,7 +12,7 @@
  */
 
 /*  Copyright (C) 2007  Vanderson Rodrigues <vanderson.gold@gmail.com>
- *
+ *  Copyright (C) 2007  Adenilson Cavalcanti <savagobr@yahoo.com> 
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
