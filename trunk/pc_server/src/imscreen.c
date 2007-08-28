f7452c252370d06c5aa325f06b5fedf1faf29bdf
cavalcantii <cavalcantii@1d5b794b-e733-0410-af9d-65fb28a70ecb>
2007-08-28 18:13:08 +0000

    Adding some Xlib related code to 'screen_capture' function, so that user only supplies a display pointer to current X session.

--- trunk/pc_server/src/imscreen.c
+++ trunk/pc_server/src/imscreen.c
@@ -29,14 +29,23 @@
 
 #include "imscreen.h"
 
-int screen_capture(Display *display, Window *window,
-		   XWindowAttributes *window_attr,
-		   Imlib_Image *image)
+int screen_capture(Display *display, Imlib_Image *image)
 {
-	int res = -1;
-	if ((!(*image)) || (!display) || (!window) || (!window_attr)
-	    || (!image))
+	int res = -1, tmp, revert_to;
+	Window window;
+	XWindowAttributes window_attr;
+
+	if ((!(*image)) || (!display))
 		goto exit;
+
+	tmp = XGetInputFocus(display, &window, &revert_to);
+	if ((tmp == BadValue) || (tmp == BadWindow))
+		goto exit;
+
+        tmp = XGetWindowAttributes(display, window, &window_attr);
+	if ((tmp == BadDrawable) || (tmp == BadWindow))
+		goto exit;
+
 
 	imlib_context_set_anti_alias(1);
 	imlib_context_set_blend(0);
@@ -45,10 +54,10 @@ int screen_capture(Display *display, Window *window,
 					       DefaultScreen(display)));
         imlib_context_set_colormap(DefaultColormap(display,
 						   DefaultScreen(display)));
-        imlib_context_set_drawable(*window);
+        imlib_context_set_drawable(window);
         *image = imlib_create_image_from_drawable((Pixmap)0, 0, 0,
-						 window_attr->width,
-						 window_attr->height, 1);
+						  window_attr.width,
+						  window_attr.height, 1);
 
 exit:
 	return res;
