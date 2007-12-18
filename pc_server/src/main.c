c837eafea86e6a62f0dae7c4e9c65e06849c452c
tmpsantos <tmpsantos@1d5b794b-e733-0410-af9d-65fb28a70ecb>
2007-12-18 18:39:56 +0000

    Merged branch:
    https://amora.googlecode.com/svn/branches/pc_server-hal -r 280:305
    
    
    Note:
    
    This merging was manually (argh!) edited to merge only new main loop
    code. All changes regarding HAL were discarded because HAL bluetooth
    dongle detection isn't portable enough. The dongle wasn't detected
    if you aren't using Debian Etch or upgrade your kernel.
    
    I will try another approach using the same idea of Gnome Bluetooth 
    Applet: DBUS + bluetooth daemon notifications. In this way, seens to 
    work in every environment.

--- pc_server/src/main.c
+++ pc_server/src/main.c
@@ -8,12 +8,10 @@
  * This app is reponsible to receive events from cellphone and send them to
  * X Server.
  * \todo
- * - \ref check_socket_validity is just a stub
- * - refactor main.c, its starting to get smelly
  * - use mkdtemp() when storing temporary screenshot
  * - catch SIGTERM or provide a way to clean exit (close sockets).
- * - forking the process when new connection is received can be a good idea.
- * - what about a heartbeat between cellphone and server?
+ * - add utests using 'check' library
+ *
  */
 
 /*  Copyright (C) 2007  Adenilson Cavalcanti <savagobr@yahoo.com>
@@ -42,11 +40,43 @@
 #include <sys/socket.h>
 #include <string.h>
 #include <time.h>
+
 #include "x11_event.h"
 #include "bluecode.h"
 #include "protocol.h"
 #include "log.h"
+#include "loop.h"
 #include "imscreen.h"
+
+
+/** Amora global struct that holds the main resources */
+struct amora_s {
+	/** Log resource */
+	struct log_resource *log;
+	/** X11 display */
+	Display *display;
+} amora;
+
+
+/** Client file descriptor callback
+ *
+ * @param client_socket the file descriptor itself
+ *
+ * @return 0 on sucess, -1 otherwise
+ *
+ */
+static int client_socket_cb(int client_socket);
+
+
+/** Server file descriptor callback
+ *
+ * @param server_socket the file descriptor itself
+ *
+ * @return 0 on sucess, -1 otherwise
+ *
+ */
+static int server_socket_cb(int server_socket);
+
 
 /** Show program usage
  *
@@ -55,17 +85,6 @@
  */
 static void show_usage(const char *path);
 
-/** Checks for client socket status, if its still valid.
- *
- * Currently, only a dummy function, its not able to detect if the client
- * is dead.
- *
- * @param client_socket The client socket descriptor.
- *
- * @return 0 when connection is ok, -1 for error.
- * \todo Write a function that really works.
- */
-static int check_socket_validity(int client_socket);
 
 /** Check for protocol commands in buffer, used by \ref process_events
  *
@@ -75,13 +94,12 @@ static int check_socket_validity(int client_socket);
  * @param length Buffer length
  * @param client_socket Client socket connection descriptor. This function will
  * take screenshots and write data back to client.
- * @param active_display Pointer to active display. This function need access to
  * display connection to grab screenshots.
  *
  * @return Number of bytes read on sucess, -1 on error, CONN_CLOSE on exit
  * (see \ref codes).
  */
-static int treat_command(char *buffer, int length, int client_socket, Display *active_display);
+static int treat_command(char *buffer, int length, int client_socket);
 
 
 /** Check for protocol commands, handle input events (mouse
@@ -91,32 +109,25 @@ static int treat_command(char *buffer, int length, int client_socket, Display *a
  * @param buffer A string buffer with commands (i.e. UP, DOWN), see
  * all_codes in \ref protocol.h
  * @param length Buffer length
- * @param active_display Pointer to active display.
- * @param log A structure of log resources, see \ref log_resource.
  * @param client_socket Client socket connection descriptor (really used by
  * \ref treat_command).
  *
  * @return Number of bytes read on sucess, -1 on error, CONN_CLOSE on exit
  * (see \ref codes).
  */
-static int treat_events(char *buffer, int length, Display *active_display,
-		 struct log_resource *log, int client_socket);
+static int treat_events(char *buffer, int length, int client_socket);
 
 
 /** Process event stream. Reads what new commands are being received
  * in socket and send them to X session.
  *
  * @param fd Socket file descriptor.
- * @param active_display Pointer to active display.
  * @param clean_up Free local allocated resources.
- * @param log A structure of log resources, see \ref log_resource.
  *
  * @return Number of bytes read on sucess, -1 on error, CONN_CLOSE on exit
  * (see \ref codes).
  */
-static int process_events(int fd, Display *active_display, int clean_up,
-		   struct log_resource *log);
-
+static int process_events(int fd, int clean_up);
 
 
 /** Main app function.
@@ -128,18 +139,10 @@ static int process_events(int fd, Display *active_display, int clean_up,
  */
 int main(int argc, char **argv)
 {
-	Display *own_display = NULL;
 	int server_socket, client_socket, channel = 10, res;
 	int clean_up = 0;
-	fd_set fd_set_socket;
-	struct timeval time_socket;
-	struct sockaddr rem_addr;
-	unsigned int opt = sizeof(rem_addr);
 	struct service_description *sd = NULL;
-	struct log_resource *log = NULL;
-	int length = 20;
-	char buffer[length], arg, *logfile = NULL;
-	time_t last_test;
+	char arg, *logfile = NULL;
 
 	if (argc > 4) {
 		show_usage(argv[0]);
@@ -166,19 +169,18 @@ int main(int argc, char **argv)
 	if (!logfile)
 		logfile = "amora.log";
 
-	memset(&rem_addr, 0, sizeof(struct sockaddr));
 
-	log = log_build_resources(logfile);
+	amora.log = log_build_resources(logfile);
 
 	if (check_device() < 0) {
-		log_message(FIL|OUT, log, "No bluetooth device/dongle available."
+		log_message(FIL|OUT, amora.log, "No bluetooth device/dongle available."
 				" Aborting...\n");
 		return -1;
 	}
 
-	own_display = construct_display(NULL);
-	if (!own_display) {
-		log_message(FIL|OUT, log, "Error creating display object!"
+	amora.display = construct_display(NULL);
+	if (!amora.display) {
+		log_message(FIL|OUT, amora.log, "Error creating display object!"
 				"Aborting...\n");
 		return -1;
 	}
@@ -186,149 +188,56 @@ int main(int argc, char **argv)
 	/* Service description registering */
 	sd = build_sd(channel);
 	if (!sd) {
-		log_message(FIL|OUT, log, "Error creating service description"
+		log_message(FIL|OUT, amora.log, "Error creating service description"
 			    "object!"
 			    "Aborting...\n");
 		return -1;
 	}
 	res = describe_service(sd);
 	if (res == -1) {
-		log_message(FIL|OUT, log, "Error registering service!"
+		log_message(FIL|OUT, amora.log, "Error registering service!"
 			    "Aborting...\n");
 		destroy_sd(sd);
 		return -1;
 	}
 
-
 	/* Socket creation */
 	server_socket = build_bluetooth_socket(channel, sd);
 	if (server_socket == -1) {
-		log_message(FIL|OUT, log, "Failed creating bluetooth conn!"
+		log_message(FIL|OUT, amora.log, "Failed creating bluetooth conn!"
 			    "Exiting...\n");
 		return -1;
 	}
 
-	log_message(FIL, log, "Bluetooth device code hci = %d\n", sd->hci_id);
-	log_message(FIL|OUT, log, "\nInitialization done, waiting cellphone"
+	loop_add(server_socket, server_socket_cb);
+
+	log_message(FIL, amora.log, "Bluetooth device code hci = %d\n", sd->hci_id);
+	log_message(FIL|OUT, amora.log, "\nInitialization done, waiting cellphone"
 		    " connection...\n");
 
 	res = listen(server_socket, 10);
 	if (res) {
-		log_message(FIL|OUT, log, "Failed listening...\n");
+		log_message(FIL|OUT, amora.log, "Failed listening...\n");
 		return -1;
 	}
 
-	while (1) {
-		log_message(FIL|OUT, log, "Entering main loop...\n");
-		client_socket = accept(server_socket,
-				       (struct sockaddr *)&rem_addr, &opt);
-		if (client_socket == -1) {
-			log_message(FIL|OUT, log, "Failed opening connection,"
-				    " exiting...\n");
-			goto exit;
-		}
 
-		client_bluetooth_id(&rem_addr, buffer);
-		log_message(FIL|OUT, log, "Accepted connection. Client is "
-			    "%s\n", buffer);
+	log_message(FIL|OUT, amora.log, "Entering main loop...\n");
+	loop();
 
-		res = process_events(client_socket, own_display, clean_up,
-				     log);
-
-		FD_ZERO(&fd_set_socket);
-		FD_SET(client_socket, &fd_set_socket);
-		time_socket.tv_sec = 5;
-		time_socket.tv_usec = 0;
-
-		last_test = time(NULL);
-		while ((res = select(client_socket + 1, &fd_set_socket, NULL,
-				     NULL, &time_socket)) != -1) {
-
-			/* Linux resets timeout */
-			FD_ZERO(&fd_set_socket);
-			FD_SET(client_socket, &fd_set_socket);
-			time_socket.tv_sec = 5;
-			time_socket.tv_usec = 0;
-
-			if (res == 1) {
-				res = check_socket_validity(client_socket);
-
-				if (res < 0) {
-
-					/* FIXME: 3x the same block code
-					 * is awful.
-					 */
-					log_message(FIL|OUT, log,
-						    "Connection no longer"
-						    "valid\n");
-
-					close(client_socket);
-					client_socket = -1;
-					break;
-				}
-
-				res = process_events(client_socket,
-						     own_display, clean_up,
-						     log);
-
-				if (res == CONN_CLOSE) {
-					log_message(FIL|OUT, log,
-						    "Client asked to close "
-						    "connection\n\n");
-					close(client_socket);
-					client_socket = -1;
-					break;
-				}
-
-				if (res == -1) {
-					log_message(FIL|OUT, log,
-						    "Client died or closed "
-						    "connection\n\n");
-					close(client_socket);
-					client_socket = -1;
-					break;
-				}
-
-				last_test = time(NULL);
-			}
-
-			res = time(NULL);
-			if ((res - last_test) > 20) {
-				log_message(FIL, log, "Timeout, check for "
-					    "socket status");
-				last_test = res;
-				res = check_socket_validity(client_socket);
-				if (res < 0) {
-					log_message(FIL|OUT, log,
-						    "Connection no longer"
-						    "valid\n");
-
-					close(client_socket);
-					client_socket = -1;
-					break;
-				}
-
-			}
-
-		}
-
-	}
-
-exit:
-	res = destroy_display(own_display);
-	log_message(FIL|OUT, log, "Done, we are closing now.\n");
+	res = destroy_display(amora.display);
+	amora.display = NULL;
+	log_message(FIL|OUT, amora.log, "Done, we are closing now.\n");
 	close(server_socket);
 	destroy_sd(sd);
-	res = process_events(client_socket = 0, own_display = NULL,
-			     clean_up = 1, log);
-	log_clean_resources(log);
+	res = process_events(client_socket = 0, clean_up = 1);
+	log_clean_resources(amora.log);
 
 	return 0;
 }
 
 
-static int process_events(int fd, Display *active_display, int clean_up,
-		   struct log_resource *log)
+static int process_events(int fd, int clean_up)
 {
 	static char *buffer = NULL;
 	const int BUF_SIZE = 300;
@@ -336,7 +245,7 @@ static int process_events(int fd, Display *active_display, int clean_up,
 	char *start, *end;
 
 	/* Call to just cleanup local allocated memory. */
-	if ((clean_up == 1) && (fd == 0) && (active_display == NULL)) {
+	if ((clean_up == 1) && (fd == 0) && (amora.display == NULL)) {
 		if (buffer != NULL)
 			free(buffer);
 
@@ -354,17 +263,16 @@ static int process_events(int fd, Display *active_display, int clean_up,
 	}
 
 	bytes_read = read_socket(fd, buffer, BUF_SIZE);
-	if (bytes_read == -1) {
-		log_message(FIL|OUT, log, "Error trying to read socket!");
+	if (bytes_read == -1 || bytes_read == 0) {
+		log_message(FIL|OUT, amora.log, "Error trying to read socket!");
 		return result;
 	}
 
-	log_message(FIL, log, "Read buffer = %s\n", buffer);
+	log_message(FIL, amora.log, "Read buffer = %s\n", buffer);
 
 	start = buffer;
 	while ((end = strchr(start, CMD_BREAK))) {
-		result = treat_events(start, (end - start), active_display,
-				      log, fd);
+		result = treat_events(start, (end - start), fd);
 		start = ++end;
 	}
 
@@ -372,8 +280,7 @@ static int process_events(int fd, Display *active_display, int clean_up,
 }
 
 
-static int treat_events(char *buffer, int length, Display *active_display,
-		 struct log_resource *log, int client_socket)
+static int treat_events(char *buffer, int length, int client_socket)
 {
 	static unsigned char mouse_event = 0, times = 0,
 		button_right = 0, button_left = 0, button_middle = 0;
@@ -382,11 +289,11 @@ static int treat_events(char *buffer, int length, Display *active_display,
 
 	/* TODO: move this whole code block to a distinct function */
 	result = ecell_button_ewindow(buffer, length);
-	log_message(FIL, log, "ecell_button = %d\n", result);
+	log_message(FIL, amora.log, "ecell_button = %d\n", result);
 	if (result == NONE) {
 
 		result = ecell_mouse_ewindow(buffer, length);
-		log_message(FIL, log, "ecell_mouse = %d\n", result);
+		log_message(FIL, amora.log, "ecell_mouse = %d\n", result);
 		switch (result) {
 		case MOUSE_MOVE:
 			mouse_event = 1;
@@ -404,13 +311,13 @@ static int treat_events(char *buffer, int length, Display *active_display,
 			button_right = button_left = 0;
 			break;
 		case MOUSE_SCROLL_UP:
-			mouse_click(result, 0, active_display);
+			mouse_click(result, 0, amora.display);
 			break;
 		case MOUSE_SCROLL_DOWN:
-			mouse_click(result, 0, active_display);
+			mouse_click(result, 0, amora.display);
 			break;
 		case NONE:
-			log_message(FIL, log, "mouse_event = %d", mouse_event);
+			log_message(FIL, amora.log, "mouse_event = %d", mouse_event);
 			if (mouse_event == 1) {
 				if (times == 0) {
 					x_mouse = atoi(buffer);
@@ -418,14 +325,14 @@ static int treat_events(char *buffer, int length, Display *active_display,
 				} else {
 					y_mouse = atoi(buffer);
 
-					log_message(FIL, log,
+					log_message(FIL, amora.log,
 						    "x = %d\ty=%d\t",
 						    x_mouse, y_mouse);
 
 					result = mouse_move(x_mouse, y_mouse,
-							      active_display);
+							      amora.display);
 					if (result == -1)
-						log_message(FIL|OUT, log,
+						log_message(FIL|OUT, amora.log,
 							    "Can't"
 							    "move mouse!");
 
@@ -434,34 +341,33 @@ static int treat_events(char *buffer, int length, Display *active_display,
 				}
 			} else {
 				result = treat_command(buffer, length,
-						       client_socket,
-						       active_display);
+						       client_socket);
 				if (result == CONN_CLOSE) {
 					mouse_event = 0;
 					times = 0;
 					goto exit;
 				} else if (result == NONE)
-					log_message(FIL|OUT, log,
+					log_message(FIL|OUT, amora.log,
 						    "Invalid event!\n");
 			}
 			break;
 		default:
 			if (button_right)
 				mouse_click(MOUSE_BUTTON_RIGHT, result,
-					    active_display);
+					    amora.display);
 			else if (button_left)
 				mouse_click(MOUSE_BUTTON_LEFT, result,
-					    active_display);
+					    amora.display);
 			else if (button_middle)
 				mouse_click(MOUSE_BUTTON_MIDDLE, result,
-					    active_display);
+					    amora.display);
 			break;
 		}
 		goto exit;
 	}
 
 	if (result != NONE) {
-		send_event(KeyPress, x_key_code[result], active_display);
+		send_event(KeyPress, x_key_code[result], amora.display);
 		goto exit;
 	}
 
@@ -471,8 +377,7 @@ exit:
 }
 
 
-static int treat_command(char *buffer, int length, int client_socket,
-		  Display *active_display)
+static int treat_command(char *buffer, int length, int client_socket)
 {
 
 	static int do_capture = 0, screen_rotate = 0,
@@ -516,7 +421,7 @@ static int treat_command(char *buffer, int length, int client_socket,
 	case SCREEN_HEIGHT:
 		break;
 	case SCREEN_TAKE:
-		tmp = screen_capture(active_display, &image);
+		tmp = screen_capture(amora.display, &image);
 		if (tmp) {
 			perror("failed screen capture!\n");
 			result = NONE;
@@ -594,16 +499,48 @@ static void show_usage(const char *path)
 	free(p);
 }
 
-static int check_socket_validity(int client_socket)
+static int client_socket_cb(int client_socket)
 {
-	int res;
-	char buffer[1];
+	int res = process_events(client_socket, 0);
 
-	/* Try to read twice */
-	res = read(client_socket, buffer, 0);
-	if (!res)
-		res = read(client_socket, buffer, 0);
+	if (res == CONN_CLOSE) {
+		log_message(FIL|OUT, amora.log,"Client asked to close connection\n");
+		loop_remove(client_socket);
+		close(client_socket);
+		client_socket = -1;
+	}
 
-	return res;
+	if (res == -1) {
+		log_message(FIL|OUT, amora.log, "Client died or closed connection\n");
+		loop_remove(client_socket);
+		close(client_socket);
+		client_socket = -1;
+	}
+
+	return 0;
 }
 
+static int server_socket_cb(int server_socket)
+{
+	char buffer[20];
+	struct sockaddr rem_addr;
+	unsigned int opt = sizeof(struct sockaddr);
+	int client_socket;
+
+	memset(&rem_addr, 0, sizeof(struct sockaddr));
+
+	client_socket = accept(server_socket,
+			(struct sockaddr *) &rem_addr, &opt);
+
+	if (client_socket == -1)
+		log_message(FIL|OUT, amora.log, "Failed opening connection,"
+				" exiting...\n");
+	else {
+		client_bluetooth_id(&rem_addr, buffer);
+		log_message(FIL|OUT, amora.log, "Accepted connection. Client"
+				" is %s\n", buffer);
+		loop_add(client_socket, client_socket_cb);
+	}
+
+	return client_socket < 0 ? -1 : 0;
+}
