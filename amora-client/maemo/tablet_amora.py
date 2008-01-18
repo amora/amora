021145b1f4eb918214b50857d1968c98434234cc
Tomaz Noleto <tnoleto@users.noreply.github.com>
2008-01-18 22:51:07 +0000

    Edje implementation of init screen (mockup).

--- amora-client/maemo/tablet_amora.py
+++ amora-client/maemo/tablet_amora.py
@@ -3,35 +3,46 @@
 import ecore
 import ecore.evas
 import edje
+import edje.decorators
+import evas
 import sys
 import os
 
-class main_app:
+WIDTH_DFL = 800
+HEIGHT_DFL = 480
+FRAMERATE_DFL = 20
+FULLSCREEN_DFL = False
+APP_TITLE = "Amora Tablet Client"
+APP_WM_INFO = ("Amora Tablet Client", "amora-tablet-client")
+
+class AmoraTabletClient(edje.Edje):
     #Default constructor
-    def  __init__(self, width = 800, height = 480, fullscreen = True,
-                 framerate = 20):
-        f = ecore.evas.SoftwareX11
-        ee = f(w = width, h = height)
-        ee.fullscreen = fullscreen
-        edje.frametime_set(1.0 / framerate)
-        #Setup UI
-        ee.title = "Amora tablet client"
-        ee.name_class = ("amora_client", "amora_client")
-        self.canvas = ee.evas
-        self.f = f
-        self.ee = ee
-    #Loads an edje file
-    def load_edje(self, path):
-        try:
-            print path
-            self.edje_obj = edje.Edje(self.canvas, file=path, group="main")
-        except Exception, e:
-            raise SystemExit("Failed to load Edje file: %s" % path)
+    def  __init__(self, canvas, width = WIDTH_DFL, height = HEIGHT_DFL,
+                 fullscreen = FULLSCREEN_DFL, framerate = FRAMERATE_DFL):
+        edje.Edje.__init__(self, canvas)
+        self.text = []
+        edje_file = os.path.splitext(sys.argv[0])[0] + ".edj"
+        self.file_set(edje_file, "main")
 
 
 
-obj = main_app()
+if ecore.evas.engine_type_supported_get("software_x11_16") and \
+   '-x11' not in sys.argv:
+    ee = ecore.evas.SoftwareX11_16(w=WIDTH_DFL, h=HEIGHT_DFL)
+else:
+    ee = ecore.evas.SoftwareX11(w=WIDTH_DFL, h=HEIGHT_DFL)
 
-#edje_file_path = os.path.join(os.path.dirname(sys.argv[0]), "main_win.edj")
-#obj.load_edje(edje_file_path)
+canvas = ee.evas
 
+obj = AmoraTabletClient(canvas)
+obj.size = canvas.size
+obj.focus = True
+obj.show()
+
+ee.data["main"] = obj
+ee.title = APP_TITLE
+ee.name_class = APP_WM_INFO
+ee.fullscreen = FULLSCREEN_DFL
+ee.show()
+
+ecore.main_loop_begin();
