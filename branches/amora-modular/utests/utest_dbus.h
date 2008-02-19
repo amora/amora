6a954f1eee49076149ed78754b61af7c7ab2fb77
tmpsantos <tmpsantos@1d5b794b-e733-0410-af9d-65fb28a70ecb>
2008-02-19 14:47:28 +0000

    Branch created to test a modular (even more) arch in amora server.

---
+++ branches/amora-modular/utests/utest_dbus.h
@@ -0,0 +1,11 @@
+#ifndef __UTEST_DBUS_H__
+#define __UTEST_DBUS_H__
+
+#include <check.h>
+
+
+TCase *dbus_tcase_create(void);
+
+
+#endif /* __UTEST_DBUS_H__ */
+
---
+++ branches/amora-modular/utests/utest_log.h
@@ -0,0 +1,10 @@
+#ifndef __UTEST_LOG_H__
+#define __UTEST_LOG_H__
+
+#include <check.h>
+
+
+TCase *log_tcase_create(void);
+
+#endif /* __UTEST_LOG_H__ */
+
