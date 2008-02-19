0bf262229de6664671cf6cd06956266276068696
cavalcantii <cavalcantii@1d5b794b-e733-0410-af9d-65fb28a70ecb>
2008-02-19 15:10:01 +0000

    Making the discovery process run in a distinct thread, this solves the freezing problem when discovering devices and also updates the screen as new devices are found.

--- trunk/amora-client/maemo/dbus_discover.py
+++ trunk/amora-client/maemo/dbus_discover.py
@@ -29,6 +29,7 @@
 import dbus
 import dbus.glib
 import gobject
+from threading import Thread
 
 main_loop = None
 
@@ -37,8 +38,9 @@ def disc_completed_signal():
     main_loop.quit()
 
 
-class dbus_bluetooth:
+class dbus_bluetooth(Thread):
     def __init__(self):
+        Thread.__init__(self)
         self.bus = dbus.SystemBus()
     def set_callback(self, f_callback):
         self.bus.add_signal_receiver(f_callback, 'RemoteNameUpdated',
@@ -49,13 +51,13 @@ class dbus_bluetooth:
                                      'org.bluez', '/org/bluez/hci0')
         self.obj = self.bus.get_object('org.bluez', '/org/bluez/hci0')
         self.adapter = dbus.Interface(self.obj, 'org.bluez.Adapter')
+    def run(self):
+        main_loop.run()
     def discover(self):
         global main_loop
         self.adapter.DiscoverDevices()
-        gobject.threads_init()
-        dbus.glib.init_threads()
         main_loop = gobject.MainLoop()
-        main_loop.run()
+        self.start()
 
 # def test(address, name):
 #         print 'Got a device: (%s, %s)' % (address, name)
