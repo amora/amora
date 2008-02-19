a15dc5e2824548a4cda84b2dcfae8d2424a87706
Tomaz Noleto <tnoleto@users.noreply.github.com>
2008-02-19 18:21:36 +0000

    Updating debian packaging to include new .py files

--- amora-client/maemo/setup.py
+++ amora-client/maemo/setup.py
@@ -2,7 +2,6 @@ from distutils.core import setup
 
 setup(name='amora',
        version='0.1',
-       scripts=['amora'],
        data_files = [
                     ('share/pixmaps',                       ['icons/26x26/amora.png']),
                     ('share/icons/hicolor/26x26/hildon',    ['icons/26x26/amora.png']),
@@ -10,5 +9,8 @@ setup(name='amora',
                     ('share/icons/hicolor/scalable/hildon', ['icons/scalable/amora.png']),
                     ('share/applications/hildon',           ['amora.desktop']),
                     ('share/amora',                         ['amora.edj']),
+					('share/amora',                         ['amora']),
+					('share/amora',                         ['btio.py']),
+					('share/amora',                         ['dbus_discover.py']),
                     ]
       )
