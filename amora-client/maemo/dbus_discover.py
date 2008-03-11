b3ade30d8255c9313c69dba9ce1f0d43674b6e3f
Adenilson Cavalcanti <cavalcantii@gmail.com>
2008-03-11 04:18:48 +0000

    Integrating D-Bus bluetooth scan devices with ecore main loop. This fixes the issue introduced in commit r474 (running discovery in another thread) since Enlightenment is not thread safe.

--- amora-client/maemo/dbus_discover.py
+++ amora-client/maemo/dbus_discover.py
@@ -5,6 +5,7 @@
 # it seems that BlueZ bluetooth.lookup_name doesn't work within the
 # Nokia N810 internet tablet.
 # The code was based on http://wiki.bluez.org/wiki/HOWTO/DiscoveringDevices
+# and on E17 CVS sample named 'test-bt-search-devices.py'
 
 '''
 /*  Copyright (C) 2008  Adenilson Cavalcanti <savagobr@yahoo.com>
@@ -27,21 +28,17 @@
 '''
 
 import dbus
-import dbus.glib
-import gobject
-from threading import Thread
-
-main_loop = None
+import e_dbus
+import ecore
 
 def disc_completed_signal():
     print 'Signal: DiscoveryCompleted()'
-    main_loop.quit()
+    ecore.idler_add(ecore.main_loop_quit)
 
-
-class dbus_bluetooth(Thread):
+class dbus_bluetooth():
     def __init__(self):
-        Thread.__init__(self)
-        self.bus = dbus.SystemBus()
+        self.main_loop = e_dbus.DBusEcoreMainLoop()
+        self.bus = dbus.SystemBus(mainloop=self.main_loop)
     def set_callback(self, f_callback):
         self.bus.add_signal_receiver(f_callback, 'RemoteNameUpdated',
                                      'org.bluez.Adapter', 'org.bluez',
@@ -51,13 +48,9 @@ class dbus_bluetooth(Thread):
                                      'org.bluez', '/org/bluez/hci0')
         self.obj = self.bus.get_object('org.bluez', '/org/bluez/hci0')
         self.adapter = dbus.Interface(self.obj, 'org.bluez.Adapter')
-    def run(self):
-        self.adapter.DiscoverDevices()
-        main_loop.run()
     def discover(self):
-        global main_loop
-        main_loop = gobject.MainLoop()
-        self.start()
+        self.adapter.DiscoverDevices()
+        ecore.main_loop_begin()
 
 # def test(address, name):
 #         print 'Got a device: (%s, %s)' % (address, name)
